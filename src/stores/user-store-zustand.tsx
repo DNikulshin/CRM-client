06858053b36dc70890020f505a28ceb3094a1e94
DMN <98643483+DNikulshin@users.noreply.github.com>
2023-12-12 01:50:42 +0300

    Update user-store-zustand.tsx

--- src/stores/user-store-zustand.tsx
+++ src/stores/user-store-zustand.tsx
@@ -4,7 +4,10 @@ import {persist} from 'zustand/middleware'
 import axios from 'axios'
 
 const instanceAxios = axios.create({
-    baseURL: 'https://erp.silinet.net/api.php'
+    baseURL: import.meta.API_URL,
+    params: {
+        key: import.meta.API_KEY
+    }
 })
 
 interface userState {
@@ -31,10 +34,8 @@ persist(
         userId: '',
         checkAuth: async (login: string, pass: string) => {
             try {
-                const responseCheckAuth = await instanceAxios(
-                    `https://erp.silinet.net/api.php`, {
+                const responseCheckAuth = await instanceAxios('', {
                         params: {
-                            key: '0QlVFA0Q',
                             cat: 'employee',
                             action: 'check_pass',
                             login,
@@ -51,7 +52,6 @@ persist(
 
                 const responseGetUserId = await instanceAxios('', {
                     params: {
-                        key: '0QlVFA0Q',
                         cat: 'employee',
                         action: 'get_employee_id',
                         'data_typer': 'login',
